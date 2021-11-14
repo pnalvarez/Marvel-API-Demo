263eddefcab50dff64b9eeb3d75e8a05fe831b8c
pnalvarez <pedronalvarez@hotmail.com>
2021-11-13 23:14:14 -0300

    initial setup

---
+++ Marvel-API-Demo/Marvel-API-DemoUITests/Marvel_API_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Marvel_API_DemoUITestsLaunchTests.swift
+//  Marvel-API-DemoUITests
+//
+//  Created by Pedro Alvarez on 13/11/21.
+//
+
+import XCTest
+
+class Marvel_API_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
---
+++ Marvel-API-Demo/Pods/Target Support Files/Pods-Marvel-API-Demo-Marvel-API-DemoUITests/Pods-Marvel-API-Demo-Marvel-API-DemoUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Marvel_API_Demo_Marvel_API_DemoUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Marvel_API_Demo_Marvel_API_DemoUITests
+@end
